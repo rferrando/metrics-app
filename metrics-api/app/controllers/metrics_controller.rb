a2930f7cf9bac976a82a32c865d0ffb3b6fb4430
rferrando <ferrando.rosa@gmail.com>
2024-07-03 14:58:48 +0200

    Adding collection routes to add more RESTful actions to perform the data aggregations. Use JSON to serialize the data that will also formatted to expose all aggregated data  by kind of metric

--- metrics-api/app/controllers/metrics_controller.rb
+++ metrics-api/app/controllers/metrics_controller.rb
@@ -1,8 +1,8 @@
-class MetricsController < ActionController::Base
+class MetricsController < ApplicationController
 
   def index
-    @metrics = Metric.all
-    render json:  @metrics
+    @metrics = Metric.select('timestamp AS period, name, value as average_value')
+    render json:  format_aggregated_data(@metrics)
   end
 
   def create
@@ -14,9 +14,34 @@ class MetricsController < ActionController::Base
     end
   end
 
+  def by_minute
+    @metrics = Metric.by_minute
+    render json: format_aggregated_data(@metrics)
+  end
+
+  def by_hour
+    @metrics = Metric.by_hour
+    render json: format_aggregated_data(@metrics)
+  end
+
+  def by_day
+    @metrics = Metric.by_day
+    render json: format_aggregated_data(@metrics)
+  end
+
   private
 
   def metric_params
     params.require(:metric).permit(:name, :value)
+  end
+
+  def format_aggregated_data(metrics)
+    #iterate over each element and build a hash
+    metrics_hash = metrics.each_with_object({}) do |metric, hash|
+      name = metric.name
+      hash[name] ||= []
+      hash[name] << { period: metric.period, average_value: metric.average_value }
+    end
+    return metrics_hash
   end
 end
--- metrics-api/config/routes.rb
+++ metrics-api/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :metrics, only: [:create, :index] do
+    collection do
+      get :by_minute
+      get :by_hour
+      get :by_day
+    end
   end
 end
