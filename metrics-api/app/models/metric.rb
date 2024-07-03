6c5a17f9cd543744d39e8eff4cbe24fbaaf675dd
rferrando <ferrando.rosa@gmail.com>
2024-07-03 14:11:14 +0200

    Enhance the metric model to perform aggregations by using scopes with queries that aggregate data by minute, hour and day

--- metrics-api/app/models/metric.rb
+++ metrics-api/app/models/metric.rb
@@ -1,4 +1,19 @@
-class Metric < ActiveRecord::Base
+class Metric < ApplicationRecord
+    scope :by_minute, -> { group_by_period(:minute, :timestamp)}
+    scope :by_hour, -> { group_by_period(:hour, :timestamp) }
+    scope :by_day, -> { group_by_period(:day, :timestamp) }
 
+    private
+
+    def self.group_by_period(period, column)
+      case period
+      when :minute
+        select("DATE_TRUNC('minute', #{column}) as period, AVG(value) as average_value, name").group("period", "name")
+      when :hour
+        select("DATE_TRUNC('hour', #{column}) as period, AVG(value) as average_value, name").group("period", "name")
+      when :day
+        select("DATE_TRUNC('day', #{column}) as period, AVG(value) as average_value, name").group("period", "name")
+      end
+    end
 end
 
