348b59e6117cbbe532983220331c481b2202f508
rferrando <ferrando.rosa@gmail.com>
2024-07-15 01:26:28 +0200

    Add explicitely the option :null set to false to the columns that are involved in a unique index, meaning that you must provide a value in order to save a record to this table.

--- metrics-api/db/migrate/20240626070000_create_metrics.rb
+++ metrics-api/db/migrate/20240626070000_create_metrics.rb
@@ -1,8 +1,8 @@
 class CreateMetrics < ActiveRecord::Migration[5.1]
     def change
       create_table :metrics do |t|
-        t.string :name
-        t.numeric :value
+        t.string :name, null: false
+        t.decimal :value, precision: 10, scale: 2, null: false
         t.datetime :timestamp, default: -> { 'CURRENT_TIMESTAMP' }
 
         t.timestamps
